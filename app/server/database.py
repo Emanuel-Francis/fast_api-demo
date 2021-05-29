07637d035600bab0db781a6f58689cd43bc88b73
Emanuel F <51766018+Emanuel-Francis@users.noreply.github.com>
2021-05-28 23:08:05 -0700

    Add files via upload

--- app/server/database.py
+++ app/server/database.py
@@ -25,3 +25,37 @@ async def retrieve_students():
     students=[]
     async for student in student_collection.find():
         student.append(student_helper(student))
+    return students
+
+#Add a new student into the database 
+async def add_student(student_data: dict) -> dict:
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        return student_helper(student) 
+
+#Retrieve a student with a matching ID
+async def retrieve_student(id:str) -> dict:
+    student = await student_collection.find_one({"_id" : ObjectId(id)})
+    if student:
+        return student_helper(student)
+
+#Update a student with a matching ID 
+async def update_student(id:str, data:dict):
+    #Return false if an empty request body is sent 
+    if len(data) < 1:
+        return False
+    student = await student_collection.find_one({"_id" : ObjectId(id)})
+    if student:
+        update_student = await student_collection.update_one(
+            {"_id" : ObjectId(id)}, {"$set" : data}
+        )
+        if update_student:
+            return True
+        return False
+
+#Delete a student from the database 
+async def delete_student(id : str):
+    student = await student_collection.find_one({"_id" : ObjectId(id)})
+    if student:
+        await student_collection.delete_one({{"_id" : ObjectId(id)}})
+        return True
